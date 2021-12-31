ee8c712f4da58415ba56d5f902bb670efcd18239
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-12-31 20:05:02 +0530

    | Yūki • Black Knights Union Project | NekoRobot
    | Yūki • Black Knights Union Project | NekoRobot | PTB:- 13.6

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -267,7 +267,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from AsunaRobot.modules.helper_funcs.handlers import (
+from NekoRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
